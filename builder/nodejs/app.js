e87955743ddb028e0517d4fac6d4ba37664ead2c
mamemomonga <mamemomonga@gmail.com>
2017-10-27 04:59:39 +0900

    順番に処理をする処理

--- builder/nodejs/app.js
+++ builder/nodejs/app.js
@@ -1,17 +1,31 @@
 #!/usr/bin/env node
 'use strict';
 
+// 定義した順番に処理を実行する
+let SerializeTask=function(tsk,fin){
+	this.tasks=tsk;
+	if(fin) { this.finish=fin } else { this.finish=function(){} }
+};
+SerializeTask.prototype={
+	run: function() {
+		let t=this;
+		if(t.tasks.length == 0) {t.finish(); return; }
+		let task=t.tasks.shift();
+		new Promise( (res,rej)=>{ task(res,rej) } ).then( function() { t.run() });
+	}
+};
+
+// 
 let app=function(){};
 app.prototype={
 	run: function() {
 		let t=this;
 		t.data={};
-		new Promise( (res,rej)=>{ t.css(res,rej) } ).then(function() {
-			new Promise( (res,rej)=>{ t.js(res,rej) } ).then(function() {
-				t.ejs();
-			})
-		});
-
+		new SerializeTask([
+			function(res,rej){ t.css(res,rej) },
+			function(res,rej){ t.js(res,rej)  },
+			function(res,rej){ t.ejs(res,rej) }
+		],function() { console.log("FINISH"); }).run();
 	},
 	css: function(resolve,reject){
 		let t=this;
@@ -36,40 +50,46 @@ app.prototype={
 		});
 
 	},
-	ejs: function() {
+	ejs: function(resolve,reject) {
 		let t=this;
 		let fs=require('fs');
+		let ejs=require('ejs');
+		let tmpl;
 
-		fs.readFile('/volumes/app/index.ejs.html','utf8', function(err,tmpl) {
-			let ejs=require('ejs');
-			{
+		new SerializeTask([
+			function(res,rej) {
+				fs.readFile('/volumes/app/index.ejs.html','utf8', function(err,data){
+					tmpl=data;
+					console.log("Read: index.ejs.html");
+					res(true);
+				});
+			},
+			function(res,rej) {
 				let data=t.data;
 				data['production']=true;
-				fs.writeFile(
-					'/volumes/var/index.prod.html',
-					ejs.render(tmpl,t.data),	
+				fs.writeFile('/volumes/var/index.prod.html',
+					ejs.render(tmpl,data),	
 					(err)=>{
 						if (err) throw err;
 						console.log("Write: index.prod.html");
+						res(true);
 					}
 				);
-			}
-			{
+			},
+			function(res,rej) {
 				let data=t.data;
 				data['production']=false;
-				fs.writeFile(
-					'/volumes/var/index.dev.html',
-					ejs.render(tmpl,t.data),	
+				fs.writeFile('/volumes/var/index.dev.html',
+					ejs.render(tmpl,data),	
 					(err)=>{
 						if (err) throw err;
 						console.log("Write: index.dev.html");
+						res(true);
 					}
-				)
+				);
 			}
-		});
-
+		],function(){ resolve() }).run();
 	}
-
 };
 
 
