d5da9c63cf652e21c499dde0d7f0a06e14675c4a
mamemomonga <mamemomonga@gmail.com>
2017-10-31 02:38:16 +0900

    スマホ向けの調整

--- builder/webserver.sh
+++ builder/webserver.sh
@@ -7,11 +7,18 @@ cd $(dirname $0)
 
 make
 
+if [ -d var/preview ]; then
+	rm -rf var/preview
+fi
+
+mkdir -p var/preview
+cp -a ../app var/preview
+cp var/index.prod.html var/preview/index.html
+
 docker run -it --rm \
 	-p 3000:3000/tcp \
 	-e DEBUG='webserver:*' \
 	-w /opt/webserver \
-	-v $(pwd)/../app:/volumes/app:ro \
-	-v $(pwd)/var:/volumes/var \
+	-v $(pwd)/var/preview:/volumes/app \
 	$DCRIMG_ND npm start
 
