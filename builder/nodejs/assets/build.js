4ff213d35c431af0049bef18037c4efd878dddb6
mamemomonga <mamemomonga@gmail.com>
2017-11-08 05:33:04 +0900

    Makefile他調整と空のマニュアル

--- builder/nodejs/assets/build.js
+++ builder/nodejs/assets/build.js
@@ -24,8 +24,14 @@ app.prototype={
 		new SerializeTask([
 			function(res,rej){ t.css(res,rej) },
 			function(res,rej){ t.js(res,rej)  },
-			function(res,rej){ t.ejs(res,rej) }
-		],function() { console.log("FINISH"); }).run();
+			function(res,rej){
+				switch( process.argv[2] ) {
+					case 'dev':    t.ejs_dev(res,rej);    break;
+					case 'prod':   t.ejs_prod(res,rej);   break;
+					case 'manual': t.ejs_manual(res,rej); break;
+				}
+			}
+		],function(){}).run();
 	},
 	css: function(resolve,reject){
 		let t=this;
@@ -34,8 +40,8 @@ app.prototype={
 		let CleanCSS = require('clean-css');
 
 		let buf="";
-		fs.readFile('/volumes/app/reset.css', 'utf8', function (err,reset) {	
-			fs.readFile('/volumes/app/index.css', 'utf8', function (err,index) {	
+		fs.readFile('/volumes/app/reset.css', 'utf8',(err,reset)=>{	
+			fs.readFile('/volumes/app/index.css', 'utf8',(err,index)=>{	
 				t.data['css']=new CleanCSS({}).minify(reset+index).styles;
 				resolve( true );
 			});
@@ -44,59 +50,72 @@ app.prototype={
 	js: function(resolve,reject) {
 		let t=this;
 		let fs=require('fs');
-		fs.readFile('/volumes/var/index.min.js', 'utf8', function (err, text) {	
+		fs.readFile('/volumes/var/index.min.js', 'utf8',(err,text)=>{	
 			t.data['js']=text;
 			resolve( true );
 		});
 
 	},
-	ejs: function(resolve,reject) {
+	ejs_prod: function(resolve,reject) {
 		let t=this;
 		let fs=require('fs');
 		let ejs=require('ejs');
-		let tmpl;
+		fs.readFile('/volumes/app/index.ejs.html','utf8',(err,tmpl)=>{
+			console.log("Read: index.ejs.html");
+			let data=t.data;
+			let minify = require('html-minifier').minify;
+			data['production']=true;
+			data['buildnum']=process.env.BUILDNUM;
+			let html=minify(ejs.render(tmpl,data),{
+				removeAttributeQuotes: true,
+				removeComments: true,
+			});
+			fs.writeFile('/volumes/var/index.prod.html',
+				html,
+				(err)=>{
+					if (err) throw err;
+					console.log("Write: index.prod.html");
+					resolve(true);
+				}
+			);
+		});
+	},
+	ejs_dev: function(resolve,reject) {
+		let t=this;
+		let fs=require('fs');
+		let ejs=require('ejs');
+		fs.readFile('/volumes/app/index.ejs.html','utf8',(err,tmpl)=>{
+			console.log("Read: index.ejs.html");
+			let data=t.data;
+			data['production']=false;
+			fs.writeFile('/volumes/var/index.dev.html',
+				ejs.render(tmpl,data),	
+				(err)=>{
+					if (err) throw err;
+					console.log("Write: index.dev.html");
+					resolve(true);
+				}
+			);
+		});
+	},
+	ejs_manual: function(resolve,reject) {
+		let t=this;
+		let fs=require('fs');
+		let ejs=require('ejs');
+		fs.readFile('/volumes/app/manual.ejs.html','utf8',(err,tmpl)=>{
+			console.log("Read: manual.ejs.html");
+			let data=t.data;
+			fs.writeFile('/volumes/var/manual.html',
+				ejs.render(tmpl,data),	
+				(err)=>{
+					if (err) throw err;
+					console.log("Write: manual.html");
+					resolve(true);
+				}
+			);
+		});
+	},
 
-		new SerializeTask([
-			function(res,rej) {
-				fs.readFile('/volumes/app/index.ejs.html','utf8', function(err,data){
-					tmpl=data;
-					console.log("Read: index.ejs.html");
-					res(true);
-				});
-			},
-			function(res,rej) {
-				let data=t.data;
-				let minify = require('html-minifier').minify;
-				data['production']=true;
-				data['buildnum']=process.env.BUILDNUM;
-				let html=minify(ejs.render(tmpl,data),{
-					removeAttributeQuotes: true,
-					removeComments: true,
-				});
-				fs.writeFile('/volumes/var/index.prod.html',
-					html,
-					(err)=>{
-						if (err) throw err;
-						console.log("Write: index.prod.html");
-						res(true);
-					}
-				);
-			},
-			function(res,rej) {
-				let data=t.data;
-				data['production']=false;
-				fs.writeFile('/volumes/var/index.dev.html',
-					ejs.render(tmpl,data),	
-					(err)=>{
-						if (err) throw err;
-						console.log("Write: index.dev.html");
-						res(true);
-					}
-				);
-			}
-		],function(){ resolve() }).run();
-	}
+
 };
-
-
 new app().run();
